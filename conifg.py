926bb312c8c0a0e5dbcb1a668c0deea7ebe26829
Upooo <aryasptnanggra@gmail.com>
2024-06-17 01:38:12 +0700

    update

---
+++ conifg.py
@@ -0,0 +1,253 @@
+# KALO LU SAMPE SINI BERARTI LU MAU COPAS KAN?
+# NAH NI REPO GUA ACAK ACAK BIAR ORANG KAYA LU TU MIKIR ANJING
+# GA CUMA MODAL COPAS YE MONYET!
+# ------------=-------------=-------------=------------- #
+# REMAKE BY @nthannieee | @nathsupport
+# NING NING PAK NING NING OYYY
+
+
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# BEBAS MAU MAKE PUNYA GUA APA PUNYA LU.
+API_ID = int(getenv("API_ID", "25263977"))
+API_HASH = getenv("API_HASH", "be92aea5d670584b62fa2ce5ac97fe9b")
+
+# INI HARUS PUNYA LU YA MEK. JANGAN BEGO BEGO BANGET.
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", "7112277413"))
+
+# INI BEBAS TOT SUKA SUKA LU AJA.
+LOGS_MAKER_UBOT = int(getenv("LOGS_MAKER_UBOT", "-1002088219645"))
+BLACKLIST_CHAT = list(map(int, getenv("BLACKLIST_CHAT", "-1002213167805").split()))
+
+# SUKA SUKA LU NYET MAU SAMPE 10000 JUGA BODOAMAT
+MAX_BOT = int(getenv("MAX_BOT", "100"))
+
+# INI HARUS YA NGENTOT
+MONGO_URL = getenv("MONGO_URL", "")
+
+# GAUSAH DI OBRAK ABRIK KECUALI LU PUNYA API NYA YA JEMBUT
+RMBG_API = getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+COMMAND = getenv("COMMAND", ".")
+PREFIX = COMMAND.split()
+OPENAI_KEY = getenv("OPENAI_KEY", "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").split()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# MAMPUS ADA DISINI ANJING MAMPUS WHAHAHHAH
+# APA NGENTOT? MAU MARAH??
+DEV = getenv("DEV", "7112277413")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TAPI BOHONG YAHAHAHAHAHAHHAHAHAH ORANG ADA DISINI JUGA IDOT BANGET SIH
+# YANG PUNYA GUA GAUSAH DI ILANGIN YA ANJING.
+# HARGAIN LAH MONYET. AH LU TAU COPAS DOANG MONYET.
+# DEVS = (7112277413)
